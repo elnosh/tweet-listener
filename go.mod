ade71ea7368f367f3bd62526ce48ce0941299b42
miguelhun <hunmiguel1124@gmail.com>
2021-05-03 23:15:33 -0500

    rendering tweets html page

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/miguelhun/tweet-listener
 go 1.16
 
 require (
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
-	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/gorilla/websocket v1.4.2
+	github.com/onsi/ginkgo v1.16.1 // indirect
+	github.com/onsi/gomega v1.11.0 // indirect
 )
--- listen.go
+++ listen.go
@@ -1,21 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/miguelhun/tweet-listener/redis"
 )
 
 func main() {
-	listenTweets()
+	http.HandleFunc("/tweetStream", printTweets)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
-func listenTweets() {
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func printTweets(w http.ResponseWriter, r *http.Request) {
+
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
 	pubsub := redis.RedisClient.Subscribe("tweetChannel")
 
 	ch := pubsub.Channel()
 
 	for msg := range ch {
-		fmt.Printf("tweet is: %s \n", msg.Payload)
+		if err = conn.WriteMessage(websocket.TextMessage, []byte(msg.Payload)); err != nil {
+			log.Println(err)
+		}
 	}
+
 }
